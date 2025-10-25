1b423f0a82adff0064d46c634c67a696815e687b
Joao Marcelo <joaomarcelo318@outlook.com>
2025-10-25 15:14:08 -0300

    feat: Initial commit

---
+++ src/main/java/com/faculdade/passig_empilhadeiras/PassigEmpilhadeirasApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.passig_empilhadeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassigEmpilhadeirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassigEmpilhadeirasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faculdade/passig_empilhadeiras/PassigEmpilhadeirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculdade.passig_empilhadeiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassigEmpilhadeirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
