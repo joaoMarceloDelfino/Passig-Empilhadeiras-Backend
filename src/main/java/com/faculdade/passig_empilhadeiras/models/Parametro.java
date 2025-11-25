99d43feea51003165ca00efa53185171a13d6569
Joao Marcelo <joaomarcelo318@outlook.com>
2025-11-24 23:22:32 -0300

    feat: Create Model tabela Parametros.

---
+++ src/main/java/com/faculdade/passig_empilhadeiras/models/Parametro.java
@@ -0,0 +1,46 @@
+package com.faculdade.passig_empilhadeiras.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Parametros")
+public class Parametro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Max(20)
+    @NotNull
+    private String nome;
+
+    @Max(50)
+    @NotNull
+    private String valor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
