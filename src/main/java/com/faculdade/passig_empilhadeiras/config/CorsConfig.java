d54eda5bd671e3e9865bf274cdafc06ba472aa06
Joao Marcelo <joaomarcelo318@outlook.com>
2025-12-01 02:16:21 -0300

    chore: Correcao cors config

--- src/main/java/com/faculdade/passig_empilhadeiras/config/CorsConfig.java
+++ src/main/java/com/faculdade/passig_empilhadeiras/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://passig-empilhadeiras-frontend.vercel.app/", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
